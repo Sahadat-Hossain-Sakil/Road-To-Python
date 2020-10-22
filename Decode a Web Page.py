909015b0c3aed3a5677519bc0790e8a89a5a9083
SAHADAT HOSSAIN SAKIL <66558834+Sahadat-Hossain-Sakil@users.noreply.github.com>
2020-10-22 14:01:14 +0600

    Initial commit

---
+++ Decode a Web Page.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+# We can use any weblink for decode, but we must specify the class 
+
+link = "https://www.theguardian.com/international" 
+r = requests.get(link).text
+soup = BeautifulSoup(r, features="html.parser")
+print("\n ---HEADLINES ARE BELOW--- \n")
+for story_line in soup.find_all(class_="u-faux-block-link__overlay js-headline-text"):
+    print(story_line.get_text().replace("\n", " ").strip())
