23208be3913caa67bac99b15fb427a4714fbc6f3
Shahadat Hossain Shakil <mohammudvai0@gmail.com>
2020-10-22 14:35:33 +0600

    Initial commit

--- Reverse Word Order.py
+++ Reverse Word Order.py
@@ -1,4 +1,4 @@
-text = str(input('Enter a long string with many number : '))
+text = str(input('Enter a long string with some word : '))
 txt_list = text.split(" ")
 for i in range(1, len(txt_list)+1):
 	print(txt_list[-i], end = ' ')
