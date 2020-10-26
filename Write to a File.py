867e0195b10d9665ebdc519b0a8c61a1abf809a7
Sahadat Hossain Sakil <sh.shakil112531@gmail.com>
2020-10-26 23:36:25 +0600

    Initial commit

--- Write to a File.py
+++ Write to a File.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Here we create a .txt file and write into it. 
+# This algorithm will decode a web page and get all file as text for some specific class in page HTML or CSS.
+# And then write into the .txt file.
+
 link = "https://www.theguardian.com/international"
 r = requests.get(link).text
 soup = BeautifulSoup(r, features="html.parser")
