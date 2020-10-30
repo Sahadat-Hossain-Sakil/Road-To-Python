09b2dcc416758431af0b4183ca5c7e3904ada0f1
Sahadat Hossain Sakil <sh.shakil112531@gmail.com>
2020-10-30 23:23:42 +0600

    Initial commit

--- Draw a game board.py
+++ Draw a game board.py
@@ -1,13 +1,17 @@
-def dot_line():
-    print(" ..."*(size-1))
+user = input(" Give a shape of game board seperate by space (3 2) or (3 3) :> ")
+x, y = user.split()
+x = int(x)
+y = int(y)
+dot_line = " ..."
+var_line = "|   "
+def x_output(x):
+	print(dot_line * x)
 
-def var_line():
-    print("|   "*size)
-size = int(input("\nEnter your game board size : "))
-size += 1
-for i in range(size):
-    dot_line()
-    if i < size-1 :
-        var_line()
-print("\n")
+def y_output(x):
+	print(var_line * (x+1))
 
+for i in range(y):
+	x_output(x)
+	y_output(x)
+
+x_output(x)
